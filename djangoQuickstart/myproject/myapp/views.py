1fd64896df99e2b9efa549b2072c17f80789fc3c
Mariya <mariyasha432@protonmail.com>
2022-08-24 08:46:17 -0700

    updated files + display images and variables

--- djangoQuickstart/myproject/myapp/views.py
+++ djangoQuickstart/myproject/myapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from datetime import date
 
 # Create your views here.
 def myview(request):
-	return render(request, "index.html")
+	# fetch the current datetime
+	today = date.today()
+	# send the current date to the template using the third argument of render
+	return render(request, "index.html", {"mydate": today})
